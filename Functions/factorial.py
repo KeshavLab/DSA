31a4eb353c9a5d2f9600d30d29f74f17140c9561
KeshavLab <kolekarkeshav38@gmail.com>
2025-12-07 13:06:11 +0530

    tt

--- Functions/factorial.py
+++ Functions/factorial.py
@@ -1,8 +1,18 @@
-def factorialNUm(num):
+# def factorialNUm(num):
+#     if num==0 or num==1:
+#         return 1
+#     else:
+#         return num*factorialNUm(num-1)
+    
+
+# print(factorialNUm(5))
+
+def factorialNum(num):
     if num==0 or num==1:
         return 1
     else:
-        return num*factorialNUm(num-1)
+        return num *factorialNum(num-1)
     
 
-print(factorialNUm(5))
+
+print(factorialNum(5))
