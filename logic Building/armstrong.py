72f24f43930abc65fc65628b0ec2327da9218a64
KeshavLab <kolekarkeshav38@gmail.com>
2025-12-10 18:09:00 +0530

    tt

---
+++ logic Building/armstrong.py
@@ -0,0 +1,12 @@
+num=int(input("enter a number :"))
+
+n=len(str(num))
+sum=0
+for digit in str(num):
+    sum +=int(digit)**n
+
+
+if sum==num:
+    print(f"{num} is armstrong number")
+else:
+    print(f"{num} is not armstrong number")
