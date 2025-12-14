bbb8b41646607cc348c6c0e805fe9f24efe31352
KeshavLab <kolekarkeshav38@gmail.com>
2025-12-14 14:54:52 +0530

    tt

--- logic Building/countDigit.py
+++ logic Building/countDigit.py
@@ -1,10 +1,18 @@
-num=int(input('enter a number :'))
+# num=int(input('enter a number :'))
 
+# count=0
+
+# while num> 0:
+#     num=num//10
+#     count=count+1
+
+
+# print(count)
+
+num=int(input("enter  a number :"))
 count=0
-
-while num> 0:
+while num>0:
     num=num//10
-    count=count+1
-
+    count=count +1
 
 print(count)
